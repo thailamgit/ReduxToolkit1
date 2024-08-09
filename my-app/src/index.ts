758d08013ef41cad2a55055312f9e4cd5e1f4aa0
Ninh Thai Lam <thailamstudy@gmail.com>
2024-08-09 11:26:43 +0700

    Sua index.ts thanh index.tsx

--- my-app/src/index.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { ReactDOM } from "react";
-import './index.css'
-import App from "./App";
-import { store } from "./app/store";
-import { Provider } from "react";
-
